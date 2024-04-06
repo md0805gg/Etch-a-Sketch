bc9d5677624ed36bbc28cfc47642af63595de4c0
md0805gg-linux <112896171+md0805gg@users.noreply.github.com>
2024-04-06 16:58:24 +0200

    Create loop that adds 16 squares per div

--- script.js
+++ script.js
@@ -4,4 +4,9 @@ for (let i = 0; i <=15; i++) {
   const rowWrapper = document.createElement('div');
   rowWrapper.setAttribute('class', 'row-wrapper');
   container.appendChild(rowWrapper);
+  for (let i = 0; i <=15; i++) {
+    const square = document.createElement('div');
+    square.setAttribute('class', 'square');
+    rowWrapper.appendChild(square);
+  }
 };
