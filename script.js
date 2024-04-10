91090b10cc89cf6ed0fb827de03328e657ad1280
md0805gg-linux <112896171+md0805gg@users.noreply.github.com>
2024-04-10 15:45:17 +0200

    Create newGrid function

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const container = document.querySelector('.container');
 
-for (let i = 0; i <=15; i++) {
-  const rowWrapper = document.createElement('div');
-  rowWrapper.setAttribute('class', 'row-wrapper');
-  container.appendChild(rowWrapper);
-  for (let i = 0; i <=15; i++) {
-    const square = document.createElement('div');
-    square.setAttribute('class', 'square');
-    rowWrapper.appendChild(square);
-  }
-};
+const newGrid = (inputValue) => {
+  for (let i = 0; i < inputValue; i++) {
+    const rowWrapper = document.createElement('div');
+    rowWrapper.setAttribute('class', 'row-wrapper');
+    container.appendChild(rowWrapper);
+    for (let i = 0; i < inputValue; i++) {
+      const square = document.createElement('div');
+      square.setAttribute('class', 'square');
+      rowWrapper.appendChild(square);
+    }
+  };
+}
+
+newGrid(16);
 
 const squares = document.querySelectorAll('.square');
 
@@ -22,6 +26,8 @@ const clearGrid = () => {
 const button = document.querySelector('button');
 
 button.addEventListener('click', clearGrid);
+
+
 
 const colorSquare = (square) => {
   square.addEventListener('mouseover', () => 
